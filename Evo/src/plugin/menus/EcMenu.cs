f1099207153dedfc40174fefb81eee63316acc81
ShookEagle <shookeagle@gmail.com>
2025-10-21 05:56:56 -0500

    Allow setting of callback function

--- Evo/src/plugin/menus/EcMenu.cs
+++ Evo/src/plugin/menus/EcMenu.cs
@@ -1,5 +1,6 @@
 ﻿using CounterStrikeSharp.API.Core;
 using Evo.api.plugin;
+using Evo.plugin.extensions;
 using Evo.plugin.menus.components;
 using Evo.plugin.menus.theme;
 using RMenu;
@@ -8,8 +9,8 @@ using RMenu.Enums;
 namespace Evo.plugin.menus;
 
 public class EcMenu : MenuBase {
-  public EcMenu(IEvo evo, CCSPlayerController player) :
-    base(new Header("EC Menu"), new Footer(), Theme.MENU_OPTIONS) {
+  public EcMenu(IEvo evo) : base(
+    new Header("EC Menu"), new Footer(), Theme.MENU_OPTIONS) {
     Items.AddRange([
       new MenuItem(MenuItemType.Button, new MenuValue("Modes")),
       new MenuItem(MenuItemType.Button, new MenuValue("Maps")),
@@ -18,5 +19,31 @@ public class EcMenu : MenuBase {
       new MenuItem(MenuItemType.Button,
         new MenuValue("Reset", Theme.ACCENT_RED.ToMenuFormat()))
     ]);
+    
+    this.SetRootCallback((menu, action) => {
+      if (action != MenuAction.Select) return;
+      if (menu.SelectedItem == null) return;
+      switch (menu.SelectedItem.Index) {
+        case 0:   //Modes
+          Menu.Display(menu.Player, new EcMenu(evo), true);
+          break;
+        case 1:   //Maps
+          Menu.Display(menu.Player, new EcMenu(evo), true);
+          break;
+        case 2:   //Settings
+          Menu.Display(menu.Player, new EcMenu(evo), true);
+          break;
+        case 3:   //Tools
+          Menu.Display(menu.Player, new EcMenu(evo), true);
+          break;
+        case 4:   //Reset
+          Menu.Display(menu.Player, new EcMenu(evo), true);
+          break;
+        default:
+          menu.Player.PrintLocalizedChat(evo.GetBase().Localizer,
+            "error_try_again", "Invalid menu selection.");
+          break;
+      }
+    });
   }
 }

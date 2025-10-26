98d6817b27e2a0583b972fb4269d701589cf8d26
ShookEagle <shookeagle@gmail.com>
2025-10-26 16:07:48 -0500

    Harden Classes

--- Evo/src/plugin/menus/models/EvoMenuBase.cs
+++ Evo/src/plugin/menus/models/EvoMenuBase.cs
@@ -9,6 +9,8 @@ namespace Evo.plugin.menus.models;
 public abstract class EvoMenuBase(string header, MenuOptions? options = null)
   : MenuBase(new Header(header), new Footer(), options ?? Theme.MENU_OPTIONS) {
   public void Show(CCSPlayerController player, bool subMenu = false) {
+    Player = player;
+    Items.Clear();
     Build();
     Menu.Display(player, this, subMenu, Callback);
   }

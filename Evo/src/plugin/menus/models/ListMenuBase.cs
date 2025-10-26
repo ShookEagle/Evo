0aea44ccdde8cb01856ca87789e1a325919dc240
ShookEagle <shookeagle@gmail.com>
2025-10-26 18:43:08 -0500

    Refactor Option Selection

--- Evo/src/plugin/menus/models/ListMenuBase.cs
+++ Evo/src/plugin/menus/models/ListMenuBase.cs
@@ -13,19 +13,14 @@ public abstract class ListMenuBase(string header, MenuOptions? options = null)
   override protected void Build() {
     foreach (var option in Options) {
       Items.Add(new MenuItem(MenuItemType.Button,
-        new MenuValue(option.Key, Theme.TEXT_PRIMARY.ToMenuFormat()),
-        data: option.Value));
+        new MenuValue(option.Key, Theme.TEXT_PRIMARY.ToMenuFormat())));
     }
   }
 
   override protected void Callback(MenuBase menu, MenuAction action) {
     if (action != MenuAction.Select || menu.SelectedItem is null) return;
-
-    var item = menu.SelectedItem.Item;
-    var data = item.Data ?? item.Head;
-    var name = item.Head?.ToString();
-
-    OnSelected(data, name ?? throw new InvalidOperationException());
+    var selected = Options.ElementAt(menu.SelectedItem.Index);
+    OnSelected(selected.Value, selected.Key ?? throw new InvalidOperationException());
   }
 
   abstract protected void OnSelected(object? data, string name);

dcd2b8085ecf227f3cfb018a9690ae1354c2f566
ShookEagle <shookeagle@gmail.com>
2025-11-01 16:32:08 -0500

    Update Settings Service

--- Evo/src/api/plugin/services/ISettingService.cs
+++ Evo/src/api/plugin/services/ISettingService.cs
@@ -5,5 +5,5 @@ namespace Evo.api.plugin.services;
 public interface ISettingService {
   IReadOnlyDictionary<string, Setting> All { get; }
   bool TryGet(string key, out Setting def);
-  bool TryGetDefault(string key, out bool value);
+  bool TrySetting(string key, bool value);
 }
--- Evo/src/plugin/services/SettingService.cs
+++ Evo/src/plugin/services/SettingService.cs
@@ -1,4 +1,5 @@
-﻿using Evo.api.plugin;
+﻿using CounterStrikeSharp.API;
+using Evo.api.plugin;
 using Evo.api.plugin.services;
 using Evo.plugin.models;
 
@@ -6,23 +7,25 @@ namespace Evo.plugin.services;
 
 public class SettingService : ISettingService {
   private readonly Dictionary<string, Setting> byKey;
+  private readonly Dictionary<string, bool> currentSettings = new();
   public IReadOnlyDictionary<string, Setting> All => byKey;
 
   public SettingService(IEvo evo) {
     var path = $"{evo.GetBase().ModulePath}/../{evo.Config.SettingsJsonPath}";
     byKey = JsonCfg.Load<Dictionary<string, Setting>>(path);
+    foreach (var kv in byKey) { currentSettings[kv.Key] = kv.Value.Default; }
   }
 
   public bool TryGet(string key, out Setting def)
     => byKey.TryGetValue(key, out def!);
 
-  public bool TryGetDefault(string key, out bool value) {
-    if (byKey.TryGetValue(key, out var def)) {
-      value = def.Default;
-      return true;
-    }
+  public bool TrySetting(string key, bool value) {
+    if (!byKey.TryGetValue(key, out var def)) return false;
 
-    value = false;
-    return false;
+    Server.ExecuteCommand(
+      $"exec settings/{def.Stem}_{(value ? "on" : "off")}.cfg");
+    currentSettings[key] = value;
+    
+    return true;
   }
 }

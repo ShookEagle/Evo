924119a2a51486a0b2db588d178509f9d959e21c
ShookEagle <shookeagle@gmail.com>
2025-11-03 16:39:01 -0600

    Create PlayerStat Model

---
+++ Evo/src/plugin/models/PlayerStat.cs
@@ -0,0 +1,7 @@
+﻿namespace Evo.plugin.models;
+
+public sealed class PlayerStat {
+  public string Name = "";
+  public TimeSpan Accumulated = TimeSpan.Zero;
+  public DateTime? JoinedAtUtc = null; // null = currently not in server
+}

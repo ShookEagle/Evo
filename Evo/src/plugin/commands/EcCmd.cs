682d871da0d603fc0910ebe80c3c67bbdf0e0ea0
ShookEagle <shookeagle@gmail.com>
2025-11-03 17:49:42 -0600

    Restrict EC Menu to Managers

--- Evo/src/plugin/commands/EcCmd.cs
+++ Evo/src/plugin/commands/EcCmd.cs
@@ -1,13 +1,23 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
 using Evo.api.plugin;
+using Evo.plugin.extensions;
 using Evo.plugin.menus;
+using MAULActainShared.maul.enums;
 
 namespace Evo.plugin.commands;
 
 public class EcCmd(IEvo evo) : Command(evo) {
   public override void OnCommand(CCSPlayerController? executor,
     CommandInfo info) {
-    if (executor != null) new EcMenu(Plugin).Show(executor);
+    if (executor != null && executor.GetRank() < MaulPermission.Manager) {
+      info.ReplyLocalized(Plugin.GetBase().Localizer, "no_permission",
+        "Manager", "rank");
+      return;
+    }
+
+    if (executor == null) return;
+
+    new EcMenu(Plugin).Show(executor);
   }
 }

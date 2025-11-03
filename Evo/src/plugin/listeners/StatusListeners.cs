01d23ee29e9713ca9c88c154c18bc3873f81a8d2
ShookEagle <shookeagle@gmail.com>
2025-11-03 16:39:25 -0600

    Create Status listeners

---
+++ Evo/src/plugin/listeners/StatusListeners.cs
@@ -0,0 +1,39 @@
+﻿using CounterStrikeSharp.API.Core;
+using Evo.api.plugin;
+using Evo.api.plugin.services;
+using Evo.plugin.extensions;
+
+namespace Evo.plugin.listeners;
+
+public class StatusListeners {
+  private readonly IStatusService statusService;
+
+  public StatusListeners(IEvo evo) {
+    statusService = evo.GetStatusService();
+
+    evo.GetBase()
+     .RegisterEventHandler<EventPlayerConnectFull>(OnPlayerConnectFull);
+    evo.GetBase()
+     .RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
+  }
+
+  private HookResult OnPlayerConnectFull(EventPlayerConnectFull @event,
+    GameEventInfo info) {
+    if (!statusService.Running) return HookResult.Continue;
+    var player = @event.Userid;
+    if (!player.IsReal() || player == null) return HookResult.Continue;
+    
+    statusService.AddPlayer(player);
+    return HookResult.Continue;
+  }
+  
+  private HookResult OnPlayerDisconnect(EventPlayerDisconnect @event,
+    GameEventInfo info) {
+    if (!statusService.Running) return HookResult.Continue;
+    var player = @event.Userid;
+    if (!player.IsReal() || player == null) return HookResult.Continue;
+    
+    statusService.OnLeave(player);
+    return HookResult.Continue;
+  }
+}

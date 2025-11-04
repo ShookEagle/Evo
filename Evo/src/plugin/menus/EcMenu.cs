5377b6a2bf19f5f844362f19b50687b5bfa9e6ba
ShookEagle <shookeagle@gmail.com>
2025-11-03 18:38:24 -0600

    implement reset menu

--- Evo/src/plugin/menus/EcMenu.cs
+++ Evo/src/plugin/menus/EcMenu.cs
@@ -1,4 +1,5 @@
-﻿using CounterStrikeSharp.API;
+﻿using System.Diagnostics.Tracing;
+using CounterStrikeSharp.API;
 using Evo.api.plugin;
 using Evo.plugin.extensions;
 using Evo.plugin.menus.models;
@@ -36,9 +37,9 @@ public class EcMenu(IEvo evo) : EvoMenuBase("EC Menu") {
       case 3:
         new ToolsMenu(evo).Show(Player, true);
         break;
-      /*case 4:
-        new EcMenu(evo).Show(Player, true);
-        break;*/
+      case 4:
+        new ResetMenu(evo).Show(Player, true);
+        break;
       default:
         menu.Player.PrintLocalizedChat(evo.GetBase().Localizer,
           "error_try_again", "Invalid Menu Operation");
---
+++ Evo/src/plugin/menus/ResetMenu.cs
@@ -0,0 +1,45 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Evo.api.plugin;
+using Evo.plugin.menus.models;
+using Evo.plugin.menus.theme;
+using RMenu;
+using RMenu.Enums;
+
+namespace Evo.plugin.menus;
+
+public class ResetMenu(IEvo evo) : EvoMenuBase("Reset Server?") {
+  override protected void Build() {
+    Items.AddRange([
+      new MenuItem(MenuItemType.Text,
+        new MenuValue(
+          "Are you sure you would like to reset the server to it's default config and map?",
+          Theme.ACCENT_RED.ToMenuFormat())),
+      new MenuItem(MenuItemType.Spacer),
+      new MenuItem(MenuItemType.Button,
+        new MenuValue("Confirm", Theme.TEXT_PRIMARY.ToMenuFormat()))
+    ]);
+  }
+
+  override protected void Callback(MenuBase menu, MenuAction action) {
+    if (menu.SelectedItem == null) return;
+    switch (menu.SelectedItem.Index) {
+      case 2:
+        resetServer(menu.Player);
+        break;
+    }
+  }
+
+  private void resetServer(CCSPlayerController player) {
+    evo.GetModeService().TrySetMode("default");
+    Server.ExecuteCommand("exec utils/unload_plugins.cfg");
+    Server.ExecuteCommand("exec utils/server_default.cfg");
+
+    evo.GetAnnouncer()
+     .Announce(player.PlayerName, string.Empty, "Reset", "the server",
+        "lightred");
+
+    evo.GetBase()
+     .AddTimer(3f, () => { Server.ExecuteCommand("changelevel de_dust2"); });
+  }
+}

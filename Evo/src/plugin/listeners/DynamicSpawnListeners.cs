3b97aee757a53ef50828c0c4ee52056aff4f8d11
ShookEagle <shookeagle@gmail.com>
2025-11-09 08:12:56 -0600

    Dynamic Spawn Listener

---
+++ Evo/src/plugin/listeners/DynamicSpawnListeners.cs
@@ -0,0 +1,45 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using Evo.api.plugin;
+using Evo.plugin.enums;
+using Evo.plugin.extensions;
+
+namespace Evo.plugin.listeners;
+
+public class DynamicSpawnListeners {
+  private readonly IEvo evo;
+
+  public DynamicSpawnListeners(IEvo evo) {
+    this.evo = evo;
+    
+    evo.GetBase()
+     .RegisterEventHandler<EventJointeamFailed>(onJoinFailed, HookMode.Pre);
+  }
+
+  private HookResult onJoinFailed(EventJointeamFailed @event,
+    GameEventInfo info) {
+    var player = @event.Userid;
+    if (player == null || !player.IsReal()) { return HookResult.Continue; }
+    var failReason = (JoinTeamReason)@event.Reason;
+
+    switch (failReason) {
+      case JoinTeamReason.TEAMS_FULL:
+        evo.GetPlayerSpawnService().TryCreateSpawn(CsTeam.Terrorist);
+        evo.GetPlayerSpawnService().TryCreateSpawn(CsTeam.CounterTerrorist);
+        break;
+      case JoinTeamReason.TERRORIST_TEAM_FULL:
+        evo.GetPlayerSpawnService().TryCreateSpawn(CsTeam.Terrorist);
+        break;
+      case JoinTeamReason.CT_TEAM_FULL:
+        evo.GetPlayerSpawnService().TryCreateSpawn(CsTeam.CounterTerrorist);
+        break;
+      case JoinTeamReason.TEAM_LIMIT:
+      case JoinTeamReason.CT_TEAM_LIMIT:
+      default:
+        break;
+    }
+    
+    return HookResult.Continue;
+  }
+}
